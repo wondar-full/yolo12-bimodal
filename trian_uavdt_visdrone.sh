82c3f549837928aff59802dcfe46d6fbc97031ab
li-zhenyi <1420022895@qq.com>
2025-11-16 16:26:09 +0800

    deleted:    CITATION.cff 	deleted:    "COCO\346\225\260\346\215\256\351\233\206\351\233\206\346\210\220\345\256\214\346\225\264\346\226\271\346\241\210.md" 	deleted:    CONTRIBUTING.md 	deleted:    DEPTH_8BIT_ISSUE_REPORT.md 	deleted:    "DepthAnythingV2\350\204\232\346\234\254\344\277\256\345\244\215\350\257\264\346\230\216.md" 	deleted:    check_class_mapping.py 	deleted:    check_dataset_ready.py 	deleted:    comprehensive_uavdt_annotation_checker.py 	deleted:    convert_uavdt_with_class_mapping.py 	modified:   data/uav-joint-rgbd.yaml 	deleted:    diagnose_depth_loading.py 	deleted:    diagnose_exp_joint_v15_failure.py 	deleted:    diagnose_joint_v13_failure.md 	deleted:    diagnose_v16_issue.py 	deleted:    fix_all_uavdt_splits.py 	deleted:    fix_uavdt_category_ids.py 	deleted:    generate_comparison_table.py 	new file:   trian_uavdt_visdrone.sh

---
+++ trian_uavdt_visdrone.sh
@@ -0,0 +1,4 @@
+python train_uav_joint.py --device 1--cfg n --batch 16 --epochs 300
+python train_uav_joint.py --device 2--cfg m --batch 16 --epochs 300
+python train_uav_joint.py --device 3--cfg l --batch 16 --epochs 300
+python train_uav_joint.py --device 4--cfg x --batch 16 --epochs 300
